3f70eb4ca203908894a53ec7051d4c4bec917bfe
Gie-sta <70339208+Gie-sta@users.noreply.github.com>
2021-06-03 14:51:31 +0300

    pradzia

---
+++ ubn-uzduotis/src/components/App.js
@@ -0,0 +1,13 @@
+import Image from './Image';
+import LoginForm from './LoginForm'
+
+function App() {
+  return (
+    <>
+      <Image />
+      <LoginForm />
+    </>
+  );
+}
+
+export default App;
---
+++ ubn-uzduotis/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Name and Password should be stored in Redux state. Proceed to next
+screen if demo/demo is enterd. </p>
+    </div>
+  )
+}
+
+export default LoginForm
