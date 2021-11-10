a59342315828103af10ecdca8225121a61d65146
Gie-sta <70339208+Gie-sta@users.noreply.github.com>
2021-11-10 13:12:43 +0200

    readme

--- space-tourism-website/src/components/App.js
+++ space-tourism-website/src/components/App.js
@@ -1,27 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import HomePage from './pages/HomePage';
 import CrewPage from './pages/CrewPage';
 import DestinationPage from './pages/DestinationPage';
 import TechnologyPage from './pages/TechnologyPage';
-import history from '../history';
+// import history from '../history';
 
 import './App.scss';
 
 const App = () => {
   return (
-    <>
-      <Router history={history}>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={HomePage} />
-          <Route path='/destination' component={DestinationPage} />
-          <Route path='/crew' exact component={CrewPage} />
-          <Route path='/technology' exact component={TechnologyPage} />
-        </Switch>
-      </Router>
-    </>
+    <BrowserRouter>
+      {/* <Router history={history}> */}
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/destination' element={<DestinationPage />} />
+        <Route path='/crew' element={<CrewPage />} />
+        <Route path='/technology' element={<TechnologyPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
